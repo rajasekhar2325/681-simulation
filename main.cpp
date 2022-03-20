5bc27b5b4db693949d823c8b373da7f324b4f614
shivaji506 <shivaji980980@gmail.com>
2022-03-20 13:04:36 +0530

    added event types code logic

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <queue>
 
+#define idle 0
+#define busy 1
 using namespace std;
 
 int no_of_cores;
@@ -16,6 +18,7 @@ double mean_timeout_time;
 double time_quantum;
 double context_switch_time;
 int max_no_threads;
+int drops;
 
 enum type_of_event
 {
@@ -23,7 +26,7 @@ enum type_of_event
     quantum_done = 2,
     departure = 3,
     timeout = 4,
-    contextSwitch =5
+    contextSwitch = 5
 };
 
 void read_config_file()
@@ -87,12 +90,13 @@ class event
 public:
     type_of_event eventType;
     double eventStartTime;
-    thread associated_thread;
-    event(type_of_event et, double est, thread t)
+    request req;
+    int thrd_id;
+    event(type_of_event et, double est, request t)
     {
         eventType = et;
         eventStartTime = est;
-        associated_thread = t;
+        req = t;
     }
 };
 
@@ -131,7 +135,7 @@ public:
     request req;
     int thread_id;
     int assigned_core_id;
-    string status;
+    // string status;
     thread()
     {
     }
@@ -140,7 +144,7 @@ public:
         req = r;
         thread_id = id;
         assigned_core_id = thread_id % no_of_cores + 1;
-        status = "idle";
+        // status = "busy";
     }
 };
 
@@ -172,17 +176,199 @@ public:
     }
 };
 
+class Buffer{
+
+public:
+    int sz;
+    queue<event> bqu;
+    Buffer(int cap)
+    {
+        sz = cap;
+    }
+
+    bool isFull()
+    {
+        if(bqu.size() < sz)
+            return true;
+        return false;
+    }
+}
+
 class core
 {
 public:
     int core_id;
     queue<thread> jobQ;
-    string state;
+    double simTime;
+    int status; // 0 - idle and 1 - busy
     core(int id)
     {
         core_id = id;
+        status = 0;
+        simTime = 0.0;
+    }
+
+    bool isEmpty()
+    {
+        if(jobQ.size() == 0)
+            return true;
+        return false;
     }
 };
+
+class Simulation
+{
+    private:
+        int numReqCompleted;
+        priority_queue<event, vector<event>, compare> eventList;
+        Buffer buf;
+        vector<core> coreList;
+        threadpool tpool;
+        double waitingTime;
+    
+    public:
+        Simulation(int cores,int no_of_req)
+        {
+            numReqCompleted = 0;
+            waitingTime = 0.0;
+            coreList.reserve(cores);
+            for(int i=0;i<cores;i++)
+                coreList[i] = core(i);
+            tpool = threadpool();
+            buf = Buffer(50);
+
+            // for (int i = 1; i <= no_of_req; i++)
+            // requestQ.push(request(i, get_random(mean_serv_time), simTime + 1, get_random(mean_timeout_time)));
+
+            for (int i = 1; i <= no_of_req; i++)
+            {
+                // thread t = tpool.getFreeThread(requestQ.front());
+                request temp(i, get_random(mean_serv_time), simTime + 1, get_random(mean_timeout_time));
+                eventList.push(event(arrival, simTime , temp));
+                // requestQ.pop();
+            }
+
+        }
+
+        void processNextEventOnCore()
+        {
+            event curr_event = eventList.top();
+            if(curr_event.eventType == arrival)
+                handleArrival();
+            
+            else if(curr_event.eventType == departure)
+                handleDeparture();
+            
+            else if(curr_event.eventType == quantum_done)
+                handleContextSwitch();
+            else
+                handleTimeout(); // need to check what logic needs to be written
+        }
+
+
+        void handleArrival()
+        {
+            event curr_event = eventList.top();
+            eventList.pop();
+            if(tpool.poolNotEmpty())
+            {
+                thread t = tpool.getFreeThread(curr_event.req);
+                curr_event.thrd_id = t.id;
+                int core_id = t.assigned_core_id;
+                if(coreList[core_id].status == idle)
+                {
+                    coreList[core_id] = busy;
+                    if(t.req.req_rem_serv_time <= time_quantum)
+                    {
+                        // schedule departure event
+                        curr_event.eventType = departure;
+                        curr_event.eventStartTime = coreList[core_id].simTime+curr_event.thread.req.req_rem_serv_time;
+                        eventList.push(curr_event);
+                        coreList[core_id].simTime += curr_event.thread.req.req_rem_serv_time;                    
+                    }
+                    else
+                    {
+                        curr_event.eventType = quantum_done;
+                        curr_event.eventStartTime = coreList[core_id].simTime+quantum_done;
+                        t.req.req_rem_serv_time -= time_quantum;
+                        eventList.push(curr_event);
+                        coreList[core_id].simTime += time_quantum;
+                    }
+                }
+                else
+                {
+                    coreList[core_id].jobQ.push(t);
+                }
+            }
+            else
+            {
+                if(buf.isFull())
+                {
+                    drops++;
+                    // TODO: next request needs to be created or not?
+                }
+                else
+                    buf.bqu.push(curr_event);
+            }
+        }
+
+        void handleDeparture()
+        {
+            // increement request complete count by 1
+            // update waiting time
+            // schedule next arrival of request
+            // free thread and assign the free thread to next request in request queue
+            event curr_event = eventList.top();
+            eventList.pop();
+            numReqCompleted++;
+            int core_id = (curr_event.thread_id%4)+1;
+            waitingTime += coreList[core_id].simTime  +    curr_event.thread.req.req_service_time  -   curr_event.thread.req.req_arrival_time;
+            curr_event.req.req_rem_serv_time = 0;
+            jobQ.pop();
+            // if jobQueue is not empty schedule nextEvent
+            if(coreList[core_id].isEmpty())
+            {
+                // TODO: what happes to simTime variable if only one request is there in each queue on next request.
+                coreList[core_id].status = idle;
+            }
+            else
+                scheduleNextEvent(core_id);
+            threadpool.addToPool(curr_event.thrd_id);
+            curr_event.type = arrival;
+            curr_event.req.req_arrival_time = coreList[core_id].simTime+mean_think_time;
+            curr_event.req.req_rem_serv_time = mean_serv_time;
+            curr_event.thrd_id = -1;
+            eventList.push(curr_event);
+        }
+
+        void scheduleNextEvent(int core_id)
+        {
+            // not able to reuse event object. so creating new event for front thread.
+            thread tmp = coreList[core_id].front();
+            eventType type;
+            double startTime=0.0;
+            if(tmp.req.req_rem_serv_time <= time_quantum)
+            {
+                type = departure;
+                startTime = coreList[core_id].simTime+tmp.req.req_rem_serv_time;
+            }
+            else
+            {
+                type = time_quantum;
+                startTime = coreList[core_id].simTime+time_quantum;
+            }
+            coreList[core_id] += context_switch_time;
+            event curr_event(type, startTime, tmp.req);
+            eventList.push(curr_event);
+        }
+
+        void handleContextSwitch()
+        {
+            event curr_event = eventList.top();
+            eventList.pop();
+            // TODO : logic for quantum done.
+        }
+}
 
 int main()
 {
@@ -206,30 +392,13 @@ int main()
 
     while (no_of_runs--)
     {
-        int numReqCompleted = 0;
-        priority_queue<event, vector<event>, compare> eventList;
-        queue<request> requestQ;
-        vector<core> coreList;
-        for (int i = 1; i <= no_of_cores; i++)
-            coreList[i] = core(i);
-        threadpool tpool = threadpool();
-        int no_of_req = 200;
-        double simTime = 0;
-        for (int i = 1; i <= no_of_req; i++)
-            requestQ.push(request(i, get_random(mean_serv_time), simTime + 1, get_random(mean_timeout_time)));
-        for (int i = 1; i <= no_of_req; i++)
-        {
-            if (tpool.poolNotEmpty())
-            {
-                thread t = tpool.getFreeThread(requestQ.front());
-                eventList.push(event(arrival, simTime + 1, t));
-                requestQ.pop();
-            }
-            else
-                break;
-        }
+        Simulation simobj(4,200); 
         while (numReqCompleted < 1000)
         {
+            // for(int i=0;i<no_of_cores;i++)
+            // {
+                processNextEventOnCore();
+            // }
             event curr_event = eventList.top();
             if (curr_event.eventType = arrival)
             {
